d69a2be8dfc02baf2e607d4716f79ee4dd3014fe
Matthew <mrknowles55@gmail.com>
2018-01-30 15:45:09 -0500

    Added dictionary for human readability and a smart function to turn the process of creating a basic html file into 1 action.

--- WebWriter.py
+++ WebWriter.py
@@ -4,21 +4,41 @@ Attempting to write a web-page in python. This involves writing Javascript, html
 """
 import os.path
 
+html_path = "html_test.html"
+
 
 class Foo:
     def __init__(self):
-        self.html_path = "html_test.html"
+        """
+
+        """
         self.file = None
+        self.file_dict = {}
 
     @staticmethod
     def get_file_exist(path):
+        """
+        Checks if the file at the given path exists.
+        :param path: string
+        :return: True/False
+        """
         return os.path.isfile(path)
 
     @staticmethod
     def get_file_type(path):
-        return path[path.find("."):]
+        """
+        Checks the file at the given path's type via string manipulation.
+        :param path: string
+        :return: string
+        """
+        return path[path.find(".")+1:]
 
     def create_file(self, path):
+        """
+        Creates a file at the given path.
+        :param path: string
+        :return:
+        """
         print("Attempting to create a new file", path)
         if self.get_file_exist(path):
             print("File", path, "already exists.")
@@ -27,6 +47,11 @@ class Foo:
         file.close()
 
     def open_file(self, path):
+        """
+        Opens file for appended writing at the given path. Saves the file object to self.file.
+        :param path: string
+        :return:
+        """
         print("Attempting to open file", path)
         if self.get_file_exist(path):
             self.file = open(path, "w+")
@@ -35,7 +60,11 @@ class Foo:
             print("File", path, "does not exist.")
 
     def close_file(self):
-        print("Attempting to close file")
+        """
+        Closes the file found in self.file.
+        :return:
+        """
+        print("Attempting to close file.")
         try:
             self.file.close()
             print("File successfully closed.")
@@ -43,6 +72,11 @@ class Foo:
             print("No file currently open with this object.")
 
     def write_from_template(self, type):
+        """
+        Writes the basic template to self.file given its type.
+        :param type: string
+        :return:
+        """
         print("Attempting to write template to file")
         try:
             if type is "html":
@@ -53,9 +87,41 @@ class Foo:
         except AttributeError:
             print("No file loaded.")
 
+    def add_file_to_dict(self, path, key):
+        """
+        Adds a file to the dictionary for human readability and potential future functionality.
+        :param path: string
+        :param key: string
+        :return:
+        """
+        self.file_dict[key] = path
+
+    def smart_create_blank_html(self, path, name):
+        print(" ----- SMART PROCESS BEGIN ----- ")
+
+        if self.get_file_type(path) == "html":
+            if self.get_file_exist(path):
+                t = input("File " + str(path) + " already exists. Revert to basic template? (Y/N)").capitalize()
+                if t == "Y":
+                    self.open_file(path)
+                    self.write_from_template("html")
+                    self.add_file_to_dict(path, name)
+                    self.close_file()
+                    print("File " + str(path) + " reverted to basic html template")
+                else:
+                    print("Aborting process. No changes made.")
+            else:
+                self.create_file(path)
+                self.open_file(path)
+                self.write_from_template("html")
+                self.add_file_to_dict(path, name)
+                self.close_file()
+                print("File " + str(path) + " created as a basic html template")
+        else:
+            print(self.get_file_type(path))
+
 
 Bar = Foo()
 
-Bar.open_file(Bar.html_path)
-Bar.write_from_template("html")
-Bar.close_file()
+Bar.smart_create_blank_html(html_path, "Main")
+print("\n\n" + str(Bar.file_dict))
